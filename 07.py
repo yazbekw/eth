6ebe1b9666277fb2dcd75c8c6ed57cccfe6ba14e
yazbekw <yazbek965@gmail.com>
2025-11-10 20:03:55 +0300

    Update 07.py

--- 07.py
+++ 07.py
@@ -36,7 +36,7 @@ INDICATOR_CONFIG = {
 
 SIGNAL_CONFIG = {
     'min_conditions': 1,
-    'use_trend_filter': True,
+    'use_trend_filter': False,
     'use_volume_filter': False,
     'min_volume_ratio': 0.8
 }
