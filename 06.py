74f52980a289818d5e0daabadd916b20d57b9f52
yazbekw <yazbek965@gmail.com>
2025-11-11 02:04:40 +0300

    Update 06.py

--- 06.py
+++ 06.py
@@ -14,8 +14,8 @@ TRADE_CONFIG = {
     'timeframe': '1h',
     'initial_balance': 200,
     'leverage': 1,
-    'stop_loss': 0.1,
-    'take_profit': 0.1,
+    'stop_loss': 0.025,
+    'take_profit': 0.05,
     'position_size': 0.1,
     'max_positions': 3,
     'paper_trading': True
