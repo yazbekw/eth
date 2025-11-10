dca9cf2245bc562fbe3dc2560ecb1c639993f193
yazbekw <yazbek965@gmail.com>
2025-11-10 19:09:58 +0300

    Update 05.py

--- 05.py
+++ 05.py
@@ -11,7 +11,7 @@ warnings.filterwarnings('ignore')
 
 # إعدادات التداول
 TRADE_CONFIG = {
-    'symbol': 'ETHUSDT',
+    'symbol': 'ADAUSDT',
     'timeframe': '1h',
     'initial_balance': 1000,
     'leverage': 3,
