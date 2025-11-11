d8c94c04dd8a3c26278c3ef4b28e024bde288c70
yazbekw <yazbek965@gmail.com>
2025-11-12 01:11:53 +0300

    Update 011.py

--- 011.py
+++ 011.py
@@ -13,9 +13,9 @@ TRADE_CONFIG = {
     'symbol': 'BNBUSDT',
     'timeframe': '4h',
     'initial_balance': 200,
-    'leverage': 1,
-    'stop_loss': 0.06,        # تخفيف إلى 2.0%
-    'take_profit': 0.04,      # تخفيف إلى 4.0%
+    'leverag': 1,
+    'stop_loss': 0.05,        # تخفيف إلى 2.0%
+    'take_profit': 0.05,      # تخفيف إلى 4.0%
     'position_size': 0.1,
     'max_positions': 3,        # صفقتين في الوقت
     'paper_trading': True,
