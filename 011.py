95675303475ce0a89ed4157b4722cabaee3ea286
yazbekw <yazbek965@gmail.com>
2025-11-11 23:42:15 +0300

    Update 011.py

--- 011.py
+++ 011.py
@@ -15,7 +15,7 @@ TRADE_CONFIG = {
     'initial_balance': 200,
     'leverage': 1,
     'stop_loss': 0.03,        # تخفيف إلى 2.0%
-    'take_profit': 0.06,      # تخفيف إلى 4.0%
+    'take_profit': 0.045,      # تخفيف إلى 4.0%
     'position_size': 0.1,
     'max_positions': 3,        # صفقتين في الوقت
     'paper_trading': True,
