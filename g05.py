2e54a8d0843d6e7a742380a966d420affafd2549
yazbekw <yazbek965@gmail.com>
2025-11-13 00:16:09 +0300

    Update g05.py

--- g05.py
+++ g05.py
@@ -17,7 +17,7 @@ warnings.filterwarnings('ignore')
 load_dotenv()
 
 # ====================== إعدادات التفعيل / الإلغاء ======================
-ENABLE_TRAILING_STOP = False
+ENABLE_TRAILING_STOP = True
 ENABLE_DYNAMIC_POSITION_SIZING = False
 ENABLE_MARKET_REGIME_FILTER = False
 ENABLE_ATR_SL_TP = False
