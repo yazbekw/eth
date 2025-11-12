ce6ead2d8f48651a172ed1147bf924f2ed036ce0
yazbekw <yazbek965@gmail.com>
2025-11-13 01:32:28 +0300

    Update g05.py

--- g05.py
+++ g05.py
@@ -38,7 +38,7 @@ def interval_to_hours(interval):
 
 # ====================== الإعدادات الأساسية ======================
 TRADE_CONFIG = {
-    'symbol': 'BNBUSDT',
+    'symbol': 'BTCUSDT',
     'timeframe': '4h',
     'initial_balance': 200,
     'leverage': 1,
